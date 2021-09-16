dcfec3f3a6ae5b1e34a3f651f92dce797c3ea8a5
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-09-16 09:58:05 +0300

    added spaces

--- hw5.py
+++ hw5.py
@@ -1,17 +1,16 @@
-print("Введите значение для 'x' и 'y' ")
-x = int(input())
-y = int(input())
-if x>0 and y>0:
+x = int(input("Введите значение для X:"))
+y = int(input("Введите значение для Y:"))
+if x > 0 and y > 0:
     print("1-я четверть")
-elif x>0 and y<0:
+elif x > 0 and y < 0:
     print("4-я четверть")
-elif x<0 and y<0:
+elif x < 0 and y < 0:
     print("3-я четверть")
-elif x<0 and y>0:
+elif x < 0 and y > 0:
     print("2-я четверть")
-elif x==0 and y>0 or x==0 and y<0:
+elif x == 0 and y > 0 or x == 0 and y < 0:
     print("Точка лежит на оси 'Y' ")
-elif x>0 and y==0 or x<0 and y==0:
+elif x > 0 and y == 0 or x < 0 and y == 0:
     print("Точка лежит на оси 'X' ")
-elif x==0 and y==0:
+elif x == 0 and y == 0:
     print("Точка лежит в начале координат")
