c4d85ef952033775c6cbf4d632507e28d06dc916
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-10-15 22:56:05 +0300

    Add files via upload

---
+++ hw2-6.py
@@ -0,0 +1,10 @@
+a = list(input("Введите строку:"))
+k = int(input("Введите номер искомой цифры:"))
+new_list = []
+for p in a:
+    if p.isdigit():
+        new_list.append(p)
+print(new_list[k-1])
+
+
+
