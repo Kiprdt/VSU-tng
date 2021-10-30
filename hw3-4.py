354b34b4bc607223ff2af482060ab3f5f5ecef1c
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-10-30 09:31:26 +0300

    Update hw3-4.py

--- hw3-4.py
+++ hw3-4.py
@@ -2,13 +2,11 @@ def func_add():
     spisok_smth = []
     add = None
     while add != "":
-        add = input("Введите данные, чтобы закончить просто нажмите 'Enter'. ")
         spisok_smth.append(add)
-    if "" in spisok_smth:
-        spisok_smth.remove("")
+        add = input("Введите данные, чтобы закончить просто нажмите 'Enter'. ")
     for x in set(spisok_smth):    # set() - преобразует в множество, удаляя дубликаты.
         y = spisok_smth.count(x)
-        print("Элемент - ",x,"\t|""\tЧастота - ",y)
+        print("Элемент - ", x, "\t|""\tЧастота - ", y)
 
 
 func_add()
