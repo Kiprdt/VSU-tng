be7c677481bda54df806488881ad70629ce83e5d
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-11-20 10:09:19 +0300

    Update hw3-6.py

--- hw3-6.py
+++ hw3-6.py
@@ -1,10 +1,14 @@
-def func():
-    n = int(input())
+def func(z):
     g = 0
-    for i in range(2, n // 2 + 1):    
-        if n % i == 0:
+    for i in range(2, z // 2 + 1):
+        if z % i == 0:
             g = g + 1
     if g == 0:
         return True
     else:
         return False
+
+
+z = int(input())
+func(z)
+
