be51c5ea5b1d499379c60ab114826db6289a0bfd
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-11-20 11:06:26 +0300

    Update hw4-2-alpha.py

--- hw4-2-alpha.py
+++ hw4-2-alpha.py
@@ -1,13 +1,19 @@
-def factorial():
-    n = int(input("Введите число."))
+def factorial(n):
     k = 1
     if n == 0:
         return 1
     else:
         for i in range(2, n + 1):
             k = k * i
-        print(k)
+        return k
 
 
-factorial()
-      
+def factorial_tests():
+    assert factorial(5) == 120
+    assert factorial(3) == 6
+    assert factorial(0) == 1
+
+
+n = int(input("Введите число."))
+print(factorial(n))
+factorial_tests()
