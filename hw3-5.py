9f1b7456528bb1c93502242d2a0e7d7771e94589
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-11-20 10:01:41 +0300

    Update hw3-5.py

--- hw3-5.py
+++ hw3-5.py
@@ -1,7 +1,11 @@
 from hw3-1 import func_add
 
 
-x = func_add()
-x = [float(i) for i in x]    # Преобразуем все элементы списка во float.
-middle = sum(x) / len(x)
-print(middle)
+def middle_func():
+    x = func_add()
+    x = [float(i) for i in x]    # Преобразуем все элементы списка во float.
+    middle = sum(x) / len(x)
+    print(middle)
+
+
+middle_func()
