38321e306e0bc919baac9e6577bf5f37250c2db9
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2021-11-22 23:55:54 +0300

    Update hw3-2.py

--- hw3-2.py
+++ hw3-2.py
@@ -1,12 +1,20 @@
 def seasons(n):
-    months = {"Зима": (1, 2, 12),
-              "Весна": (3, 4, 5),
-              "Лето": (6, 7, 8),
-              "Осень": (9, 10, 11)}
-    for key in months:    # Перебор ключей в словаре.
-        if n in months[key]:    # Соответствие между введённым "n" и ключами.
-            return print(key)
+    months = {"Зима": [1, 2, 12],
+              "Весна": [3, 4, 5],
+              "Лето": [6, 7, 8],
+              "Осень": [9, 10, 11]}
+    for key in months:
+        if n in months[key]:
+            return key
+
+
+def seasons_tests():
+    assert seasons(4) == "Весна"
+    assert seasons(11) == "Осень"
+    assert seasons(6) == "Лето"
+    assert seasons(1) == "Зима"
 
 
 n = int(input("Введите номер месяца:"))
-seasons(n)
+print(seasons(n))
+seasons_tests()
