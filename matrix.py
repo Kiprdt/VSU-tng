bd32f5d973bdd6318082dfc3fd97227994f6c98b
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2022-02-26 12:31:29 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,25 +1,17 @@
 class Matrix:
-    def __init__(self, line=0, column=0, elements=0):
+    def __init__(self, line=0, column=0):
         self.line = line
         self.column = column
-        self.elements = elements
         self.spisok = []
 
     def input(self):
         self.line = int(input("Введите количество строк - "))
         self.column = int(input("Введите количество столбцов - "))
-        if self.line > 3 or self.column > 3:
-            print("Максимальная матрица - 3 x 3")
-            exit()
-        else:
-            print("Заполните строки.")
-        while len(self.spisok) != self.line:
+        for _ in range(self.line):
             self.spisok.append([])
-            while len(self.spisok[-1]) != self.column:
-                self.elements = int(input("Ввод - "))
-                self.spisok[-1].append(self.elements)
-        else:
-            return
+            for _ in range(self.column):
+                x = int(input("Ввод - "))
+                self.spisok[-1].append(x)
 
     def __str__(self):
         return "\n".join(["\t".join(map(str, i)) for i in self.spisok])
