f65aa067be2c04915fdf537dd493e99a56a3be0c
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2022-04-09 22:21:41 +0300

    Update matrix_tests.py

--- tests/matrix_tests.py
+++ tests/matrix_tests.py
@@ -1,21 +1,20 @@
-from app.matrix_deter import Matrix3x3
+from app.matrix import Matrix
 import pytest
 
 
-def test_determinant():
-    m = Matrix3x3([[1, 9, -8], [21, 0 , 0], [0, 4, 8]])
-    f = m.determinant()
-    assert f == -2184
-
-
-def test_determinant2():
-    m = Matrix3x3([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    f = m.determinant()
-    assert f == 0
+def test_init():
+    a = Matrix()
+    assert a.spisok == [[0]]
 
 
 def test_input(mocker):
-    mocker.patch("builtins.input", side_effect=['3', '3'])
-    f = Matrix3x3()
+    mocker.patch('builtins.input', side_effect=['3', '3', '1', '1', '1', '1', '2', '3', '4', '5', '6'])
+    f = Matrix()
     f.input()
-    assert f == Matrix3x3([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    assert f.spisok == [[1, 1, 1], [1, 2, 3], [4, 5, 6]]
+
+
+def test_str():
+    z = Matrix([[2, 1, 8], [3, 4, 6]])
+    q = str(z)
+    assert q == '2\t1\t8\n3\t4\t6'
