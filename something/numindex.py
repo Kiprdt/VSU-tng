2ce278a295cbf8905fca901220d8a2189e9cf469
Kiprdt <90186481+Kiprdt@users.noreply.github.com>
2022-04-30 18:56:09 +0300

    Update numindex.py

--- something/numindex.py
+++ something/numindex.py
@@ -1,18 +1,18 @@
-def tgr(numbers, target):
-    for i in range(len(numbers) - 1):
-        a = target - numbers[i]
-        if a in numbers:
-            if i != numbers.index(a):
-                return i, numbers.index(a)
-            else:
-                knowledge = i
-                numbers[i] = 'None'
-                for j in range(len(numbers) - 1):
-                    return knowledge, numbers.index(a)
+def mob(numbers, target):
+    chart = {}
+    for i in range(len(numbers)):
+        discovery = target - numbers[i]
+        if discovery in chart and chart[discovery] != i:
+            return chart[discovery], i
+        chart[numbers[i]] = i
 
 
-assert tgr([2, 3, 7, 4, 9], 7) == (1, 3)
-assert tgr([1, 5, 12, 2, 6], 13) == (0, 2)
-assert tgr([3, 2, 3], 6) == (0, 2)
-assert tgr([3, 3], 6) == (0, 1)
-assert tgr([3, 3, 2, 1, 3], 6) == (0, 1)
+assert mob([2, 4, 7, 9, 3], 7) == (1, 4)
+assert mob([3, 2, 3], 6) == (0, 2)
+assert mob([2, 3, 7, 4, 9], 7) == (1, 3)
+assert mob([1, 5, 12, 2, 6], 13) == (0, 2)
+assert mob([3, 2, 3], 6) == (0, 2)
+assert mob([3, 3], 6) == (0, 1)
+assert mob([3, 3, 2, 1, 3], 6) == (0, 1)
+assert mob([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) == (0, 1)
+assert mob([45, 92, 35, 21, 14, 2, 7], 9) == (5, 6)
