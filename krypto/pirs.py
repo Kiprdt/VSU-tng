3bb9e6baa9be34e2998b3ef2a2a8bcce790dffd3
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-10-07 11:31:30 +0600

    Update pirs.py

--- krypto/pirs.py
+++ krypto/pirs.py
@@ -14,3 +14,32 @@ def pirs(message, message1):
     if hasher1 == hasher2:
         return True
     return False
+
+----------------------------------------------------------------------
+
+
+import random
+
+key = 10101
+random.seed(key)
+table = list(range(0, 255))
+
+random.shuffle(table)
+
+
+def hasher(message, tabler):
+    hasher1 = len(message) % 256
+    for i in message:
+        hasher1 = tabler[(hasher1 + ord(i)) % 256]
+    return hasher1
+
+
+
+
+alp = "ABCDEFG"
+
+gen = ["A", "B", "C", "AA", "BB", "CC", "AB", "BC", "AC", "BC", "BA", "CA", "CB"]
+
+for j in gen:
+    print(hasher(j, table))
+    #если есть одинаковые то значит есть коллизия
